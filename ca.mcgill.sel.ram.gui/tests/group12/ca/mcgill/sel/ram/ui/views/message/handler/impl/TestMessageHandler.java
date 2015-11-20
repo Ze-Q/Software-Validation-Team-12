1d67480b6ec0dfbef11399ec8574e1f5c3a52f35
yazami <yahya.azami@mail.mcgill.ca>
2015-11-20 17:36:47 -0500

    Sample test for MessageHandler. Going to work on MessageHandlerView now.

--- ca.mcgill.sel.ram.gui/tests/group12/ca/mcgill/sel/ram/ui/views/message/handler/impl/TestMessageHandler.java
+++ ca.mcgill.sel.ram.gui/tests/group12/ca/mcgill/sel/ram/ui/views/message/handler/impl/TestMessageHandler.java
@@ -7,11 +7,20 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mt4j.input.inputProcessors.componentProcessors.tapAndHoldProcessor.TapAndHoldEvent;
 
 public class TestMessageHandler {
 
+   
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
+        
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }  
+        
     }
 
     @AfterClass
@@ -20,15 +29,23 @@ public class TestMessageHandler {
 
     @Before
     public void setUp() throws Exception {
+        
+        
+        
+        
     }
-
+    
+    @Test
+    public void testLine61_isHoldComplete() {
+        
+       TapAndHoldEvent myTabAndHoldEvent = new TapAndHoldEvent(null, 0, null, null, false, null, 0, 0, 0);
+       assertEquals(false, myTabAndHoldEvent.isHoldComplete());
+        
+    }
+    
     @After
     public void tearDown() throws Exception {
     }
 
-    @Test
-    public void test() {
-        fail("Not yet implemented");
-    }
-
+  
 }
