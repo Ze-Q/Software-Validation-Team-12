9ddf5b5e4ff186bb774f415fb3c440ed09c650c7
Cheng Gong <cheng.gong@mail.mcgill.ca>
2015-11-20 18:42:49 -0500

    Add setUpBeforeClass and setup methods to the test according to the TA template to allow GUI to be fully initialized before moving on to the actual test cases

--- ca.mcgill.sel.ram.gui/tests/group12/ca/mcgill/sel/ram/ui/views/message/handler/impl/TestMessageViewHandler.java
+++ ca.mcgill.sel.ram.gui/tests/group12/ca/mcgill/sel/ram/ui/views/message/handler/impl/TestMessageViewHandler.java
@@ -1,17 +1,61 @@
 package group12.ca.mcgill.sel.ram.ui.views.message.handler.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
+import org.jodah.concurrentunit.Waiter;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mt4j.sceneManagement.ISceneChangeListener;
+import org.mt4j.sceneManagement.SceneChangeEvent;
+
+import ca.mcgill.sel.commons.emf.util.AdapterFactoryRegistry;
+import ca.mcgill.sel.commons.emf.util.ResourceManager;
+import ca.mcgill.sel.ram.Aspect;
+import ca.mcgill.sel.ram.RamPackage;
+import ca.mcgill.sel.ram.provider.RamItemProviderAdapterFactory;
+import ca.mcgill.sel.ram.ui.RamApp;
+import ca.mcgill.sel.ram.ui.scenes.DisplayAspectScene;
+import ca.mcgill.sel.ram.util.RamResourceFactoryImpl;
+
+
 
 public class TestMessageViewHandler {
+	
+	private static Waiter waiter = new Waiter();
+    
+    private static Aspect aspect;
 
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
+    	// Initialize ResourceManager.
+        ResourceManager.initialize();
+        // Initialize packages.
+        RamPackage.eINSTANCE.eClass();
+        
+        // Register resource factories.
+        ResourceManager.registerExtensionFactory("ram", new RamResourceFactoryImpl());
+    
+        // Initialize adapter factories.
+        AdapterFactoryRegistry.INSTANCE.addAdapterFactory(RamItemProviderAdapterFactory.class);
+        
+        RamApp.initialize(new Runnable() {
+            
+            @Override
+            public void run() {
+                waiter.resume();
+            }
+        });
+        
+        // Wait for RamApp to be initialized.
+        try {
+			waiter.await();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     @AfterClass
@@ -20,6 +64,41 @@ public class TestMessageViewHandler {
 
     @Before
     public void setUp() throws Exception {
+        // Close current aspect.
+        if (aspect != null) {
+            RamApp.getApplication().invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    RamApp.getApplication().closeAspectScene(RamApp.getActiveAspectScene());                    
+                }
+            });
+        }
+        
+        // Load model to use in test.
+        aspect = (Aspect) ResourceManager.loadModel("/path/to/my/Model.ram");
+        
+        RamApp.getApplication().addSceneChangeListener(new ISceneChangeListener() {
+            
+            @Override
+            public void processSceneChangeEvent(SceneChangeEvent sc) {
+                if (sc.getNewScene() instanceof DisplayAspectScene) {
+                    RamApp.getApplication().removeSceneChangeListener(this);
+                    waiter.resume();
+                }
+            }
+
+			
+        });
+        
+        RamApp.getApplication().loadAspect(aspect);
+        
+        // Wait for UI to be updated.
+        try {
+			waiter.await();
+		} catch (Throwable e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 
     @After
