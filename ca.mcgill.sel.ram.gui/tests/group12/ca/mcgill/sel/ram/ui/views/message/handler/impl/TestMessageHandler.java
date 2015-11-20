e8aea72c4ffece2119bfea0b3192487343ba37ba
yazami <yahya.azami@mail.mcgill.ca>
2015-11-20 15:43:14 -0500

    MessageHandler and MessageViewHandler test classes.

---
+++ ca.mcgill.sel.ram.gui/tests/group12/ca/mcgill/sel/ram/ui/views/message/handler/impl/TestMessageHandler.java
@@ -0,0 +1,34 @@
+package group12.ca.mcgill.sel.ram.ui.views.message.handler.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TestMessageHandler {
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownAfterClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
